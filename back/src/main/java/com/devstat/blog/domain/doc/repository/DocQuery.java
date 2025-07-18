2f641fed75500417d77c380e4869600e810de9e0
ABCD <ABCtoDev@gmail.com>
2025-07-18 16:00:25 +0900

    [Refact] 필요없는 주석 삭제

--- back/src/main/java/com/devstat/blog/domain/doc/repository/DocQuery.java
+++ back/src/main/java/com/devstat/blog/domain/doc/repository/DocQuery.java
@@ -1,11 +1,8 @@
 package com.devstat.blog.domain.doc.repository;
 
-import static com.devstat.blog.domain.doc.entity.QDoc.doc;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
